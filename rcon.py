6c73fa6e9487cd54bfc4945aa46fdd01061e3251
Joshua <9244335+BuckarewBanzai@users.noreply.github.com>
2019-12-05 22:53:56 -0500

    Create rcon.py

---
+++ rcon.py
@@ -0,0 +1,26 @@
+#pip install websocket-client
+
+from websocket import create_connection
+
+password = ""
+server = ""
+port = "11776"
+
+try:
+    ws = create_connection("ws://" + server + ":" + port, subprotocols=["dew-rcon"])
+    print("Connected!")
+    ws.send(password)
+    print("Authenticated!")
+except Exception as e:
+    print("Failed to connect.")
+    print(e)
+    exit()
+
+while True:
+    n = input("\nTerminal: ")
+    print(n)
+    if n == "exit":
+        ws.close()
+    ws.send(n)
+    result =  ws.recv()
+    print("Received '%s'" % result)
