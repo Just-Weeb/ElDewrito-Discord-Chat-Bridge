621eda8e27f8f013bd1905847a6b975d4fabf1c5
Joshua <9244335+BuckarewBanzai@users.noreply.github.com>
2020-08-06 00:35:36 -0400

    Update discord-chat-bridge.py

--- discord-chat-bridge.py
+++ discord-chat-bridge.py
@@ -1,21 +1,21 @@
 import discord
+import time
 import threading
 from websocket import create_connection
 from discord_webhook import DiscordWebhook
 
+###########################  <Config>  ###################################
+password = '' #ed management password
+server = '' #ed server address
+port = '11776'
+apiToken = '' #discord bot token
+botName = '' #WEBHOOK NAME MUST MATCH BOT NAME
+discordChan = 'rcon' #Channel you have configured the webhook on 
+serverAdmin = '' #if anyone mentions a keyword notify the server admin in discord
+webHook = '' #webhook url
+###########################  </Config>  ###################################
 
-###########################  Config  ###################################
-password = '' #Eldewrito server rcon password
-server = '' #Eldewrito server address
-port = '11776' #Eldewrito server rcon port
-apiToken = '' #Your discord bot API token
-botName = '' #The name you gave your webhook bot. This is import or else the chat bridge will enter a spam loop
-discordChan = '' #Channel you want to bridge to your Eldewrito server
-serverAdmin = '<@12345678910111213>' #your user id if you want notifications enabled (You will have to turn on discord developer mode to get this. Role ID's work as well.)
-webHook = '' #Discord server webhook url
-keywords = ['admin', 'hack', 'hacker', 'server', 'Admin'] #Keywords you would like to be notified on
-##########################  Config  ###################################
-
+keywords = ['admin', 'hack', 'hacker', 'server', 'Admin']
 
 #Build our dewrito rcon connection
 def connectSock():
@@ -29,22 +29,25 @@ def connectSock():
         print("Failed to connect.")
         print(e)
 
-#Initiate our dewrito rcon connection
+#initiate our dewrito rcon connection
 connectSock()
 
 
-#Thread to handle sending messages from dewrito to discord
+#thread to handle sending messages from dewrito to discord
 def chatTX():
     while True:
         try:
             result =  ws.recv()
         except:
             connectSock()
-            continue
+            break
 
         result = result.strip('<SERVER/0000000000000000/127.0.0.1>')
 
         if '<discord>' in result:
+            continue
+
+        elif 'Join us on discord!' in result:
             continue
 
         else:
@@ -57,36 +60,51 @@ def chatTX():
             response = webhook.execute()
 
 
-#Start our dewrito to discord bridge
+def motd():
+    while True:
+
+        try:
+            ws.send("server.say Join us on discord! <discord link here>")
+
+        except:
+            continue
+
+        time.sleep(1800)
+
+#start our dewrito to discord bridge
 x = threading.Thread(target=chatTX)
 x.start()
 
+#MOTD thread
+y = threading.Thread(target=motd)
+y.start()
 
-#Start our discord to dewrito bridge
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged in as')
-        print(self.user.name)
-        print(self.user.id)
-        print('------')
+client = discord.Client()
 
-    async def on_message(self, message):
-        # we do not want the bot to reply to itself
-        if message.author.id == self.user.id:
-            return
-        elif message.author.name == botName:
-            return
-        elif message.channel.name != discordChan:
-            return
-        elif '!' in message.content[0]:
-            ws.send(message.content[1:])
-            print(message.content[1:])
-        else:
-            try:
-                ws.send('server.say <discord>{0.author}:{0.content}'.format(message))
-            except Exception as e:
-                print("Failed to connect, retrying connection...")
-                connectSock()
+@client.event
+async def on_ready():
+    print('ohai {0.user}'.format(client))
 
-client = MyClient()
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    elif message.author.name == botName:
+        return
+
+    elif message.channel.name != discordChan:
+        return
+
+    elif '!' in message.content[0]:
+        ws.send(message.content[1:])
+        print(message.content[1:])
+
+    else:
+        try:
+            ws.send('server.say <discord>{0.author}:{0.content}'.format(message))
+        except Exception as e:
+            print('Failed to connect, retrying...')
+            connectSock()
+
 client.run(apiToken)
